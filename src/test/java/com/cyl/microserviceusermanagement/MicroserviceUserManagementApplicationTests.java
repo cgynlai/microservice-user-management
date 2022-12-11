771296a784c9e81694b6db4a5c1d255540d878c1
cgyn <cgynlai@gmail.com>
2022-12-11 15:42:22 +0800

    first commit

---
+++ src/test/java/com/cyl/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyl.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
